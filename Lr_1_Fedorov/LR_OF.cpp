287905063bf6faeba248b40a14df765127d04fd2
IlyaFedorov115 <42892348+IlyaFedorov115@users.noreply.github.com>
2019-09-14 19:04:23 +0300

    Update and rename dfsfg.cpp to LR_OF.cpp

--- Lr_1_Fedorov/LR_OF.cpp
+++ Lr_1_Fedorov/LR_OF.cpp
@@ -18,12 +18,12 @@ class Vector {
 
 
 ostream &operator<<(ostream &stream, Vector obj){
-    int i = 0;
+    int i = 0; stream << "(";
     for (i; i < obj.size-1 ; i++)
     {
         stream << obj.data[i] << ", ";
     }
-    stream << obj.data[i] << "\n";
+    stream << obj.data[i] << ")"<<"\n";
     return stream;
 }
 
@@ -58,14 +58,16 @@ void readFile(const char *fileName, Vector &obj, int number){
       cout << "Считанный из файла вектор: " << obj << "\n";
 }
 
+
 void recDepth(Vector &obj, int pos , int length, int depth){
-    cout <<"\n\n" << "Глубина " << depth;
-    cout <<"\n" << "Шаг "<< step << "  Вектор = "<< obj << "\n"; step++;
+    cout <<"\n\n" << "\033[1;32mГлубина \033[0m" << depth;
+    cout <<"\n" << "Шаг "<< step << "  Вектор = "<< obj; step++;
     cout << "Обрабатываемая часть : " ;
     for (int i = pos; i < pos+length; i++)
         cout << obj.data[i] << " ";
     cout << "\n";    
 }
+
 
 void transformVector(Vector &obj, int pos , int length,int depth = 0){
     recDepth(obj, pos, length, depth);
@@ -122,14 +124,14 @@ void toLower(string &str){
 
 void readConsole(Vector &obj){
   string str;
-  cout << "Введите координаты вектора: \n";
+  cout << "\033[1;31Введите координаты вектора: \033[0m\n";
   for (int i = 0; i < obj.size ;i++)
     {
        cout << i+1 << "-e число: ";
        cin >> str;
        while (!isDigit(&str[0])) // можно и стринг
          {
-            cout << "Ошибка ввода. Попробуйте еще раз ввести число: ";
+            cout << "\033[1;31mОшибка ввода. Попробуйте еще раз ввести число: \033[0m";
             cin >> str;
          }
        obj.data[i] = atoi(str.c_str());
@@ -149,16 +151,16 @@ int main() // на всякий случай
         cout << "Введите длинну вектора: ";
         cin >> str;
         while (!isDigit(&str[0])){
-            cout << "Ошибка ввода. Попробуйте еще раз ввести число: ";
+            cout << "\033[1;31mОшибка ввода. Попробуйте еще раз ввести число: \033[0m";
             cin >> str;
         }
         obj.size = atoi(str.c_str()); // !!! Почитать, создает ли новую строку
         obj.data = new int[obj.size];
-        cout << "Если вы хотите считывать из файл, то введите \'file\' , если с консоли, то введите \'con\': ";
+        cout << "Если вы хотите считывать из файл, то введите \033[1;34m\'file\' , \033[0mесли с консоли, то введите \033[1;34m\'con\': \033[0m";
         cin >> str; toLower(str);
         while (str != file && str!=con)
           {
-            cout << "Ошибка ввода, попробуйте снова: " ;
+            cout << "\033[1;31mОшибка ввода, попробуйте снова: \033[0m" ;
             cin >> str;
           }
           if (str == file)
@@ -172,8 +174,8 @@ int main() // на всякий случай
             readConsole(obj);
              }  
         transformVector(obj, 0, obj.size, 1);
-        cout << obj;
-        cout << "\n" << "Введите \'next\' для продолжения или \'end\' для выхода: "; //любой символ
+        cout << "\n" << "Результат: " << obj;
+        cout << "\n" << "Введите \033[1;34m\'next\' \033[0m для продолжения или \033[1;34m\'end\' \033[0mдля выхода: "; //любой символ
         
         cin >> str; 
         cout << "\n";
