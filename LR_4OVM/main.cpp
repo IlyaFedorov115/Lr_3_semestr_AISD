a5a1025a61699daebaa21efc76df8e5fb7d69c60
IlyaFedorov115 <42892348+IlyaFedorov115@users.noreply.github.com>
2019-10-27 13:27:06 +0300

    Update main.cpp

--- LR_4OVM/main.cpp
+++ LR_4OVM/main.cpp
@@ -5,11 +5,12 @@
 
 int main()
 {
-    wchar_t str[] = L"Хеллоу";
+    wchar_t str[] = L"ХеLлоу";
     wchar_t str2[100];
     setlocale(LC_ALL, "");
     wscanf(L"%ls",str2);
     wprintf(L"Это есть русский языка: \"%ls\"\n", str);
     wprintf(L"Это есть русский языка2222: \"%ls\"\n", str2);
+    wprintf(L"Это буква: %lc %lc", str[2], str[3]);
     return 0;
 }
